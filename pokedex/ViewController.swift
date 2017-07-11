e95396b89ac8377707757b4ff4b211c6ea24faf3
Varun Chawla <varunchawla@Varuns-iMac.local>
2017-07-11 16:32:37 +0530

    First commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Varun Chawla on 20/04/1939 Saka.
+//  Copyright © 1939 Saka Varun Chawla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
